5758fb6ccf415e32790f2a36f2df17b43aaf3101
kharajch <kharaj2007@gmail.com>
2026-04-20 16:59:52 +0530

    feat: add next-env.js for Next.js type support in JavaScript projects

---
+++ next-env.js
@@ -0,0 +1,8 @@
+// This file was converted from next-env.d.ts to next-env.js
+// Next.js uses these references for type support even in JavaScript projects.
+
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: The following import is specific to typed routes and is commented out 
+// for JavaScript compatibility.
